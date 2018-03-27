1e4292dca52710f8c1f917e0a0b8e5e831f9d397
Agranom <boyko330@gmail.com>
2018-03-27 20:25:21 +0300

    add timepicker module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import {NgModule} from '@angular/core';
 
 
 import {AppComponent} from './app.component';
-import {NgxMaterialTimepickerModule} from "./material-timepicker/ngx-material-timepicker.module";
 import {ExampleSourceCodeComponent} from './example-source-code/example-source-code.component';
+import {NgxMaterialTimepickerModule} from './material-timepicker/ngx-material-timepicker.module';
 
 
 @NgModule({
