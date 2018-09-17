6d5d0cc790f3d78499a1c06a69336316d698fa74
Agranom <boyko330@gmail.com>
2018-09-17 22:24:04 +0300

    ref(time adapter): set permanent time format

--- src/app/material-timepicker/services/time-adapter.ts
+++ src/app/material-timepicker/services/time-adapter.ts
@@ -8,7 +8,7 @@ export class TimeAdapter {
 
     static formatTime(time: string, format = 12): string {
         const timeFormat = format === 24 ? TimeFormat.TWENTY_FOUR : TimeFormat.TWELVE;
-        return moment(time, timeFormat).format(timeFormat);
+        return moment(time, TimeFormat.TWELVE).format(timeFormat);
     }
 
     static convertTimeToMoment(time: string): Moment {
