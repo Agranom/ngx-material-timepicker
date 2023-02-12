e0ab9a6835cef80d9d6f2178e5f9ee0708e52b15
william ratte <williamratte@gmail.com>
2023-02-12 12:32:03 -0500

    Fix Invalid DateTime
    The default numbering system "latn" used to parse the time in locale string returns a Narrow No-Break Space (NNBSP) instead of standard space which cause the parse to fail. The proposed fix replace the NNBSP for a standard space.

--- src/app/material-timepicker/services/time-adapter.ts
+++ src/app/material-timepicker/services/time-adapter.ts
@@ -31,13 +31,13 @@ export class TimeAdapter {
                 ...DateTime.TIME_SIMPLE,
                 hour12: format !== 24,
                 numberingSystem: TimeAdapter.DEFAULT_NUMBERING_SYSTEM
-            }).replace(/\u200E/g, '');
+            }).replace(/\u200E/g, '').replace(' ', ' ');
         }
         return parsedTime.toISOTime({
             includeOffset: false,
             suppressMilliseconds: true,
             suppressSeconds: true
-        }).replace(/\u200E/g, '');
+        }).replace(/\u200E/g, '').replace(' ', ' ');
     }
 
     static toLocaleTimeString(time: string, opts: TimeOptions = {}): string {
